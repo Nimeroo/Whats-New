f04d004a5e6b1f466bdb601f154e7f8734e44403
Nimeroo <girou.joe@gmail.com>
2021-10-21 17:03:43 -0400

    search bar and searched articles rendered on home page

--- client/src/Screens/ArticleHome/ArticleHome.jsx
+++ client/src/Screens/ArticleHome/ArticleHome.jsx
@@ -1,28 +1,42 @@
-import "./ArticleHome.css"
-import { useEffect, useState } from 'react';
-import { getArticles, getSearchedArticles } from '../../Services/api-config';
+import "./ArticleHome.css";
+import { useEffect, useState } from "react";
+import { getArticles, getSearchedArticles } from "../../Services/api-config";
 import Articles from "../../Components/Articles/Articles";
 
 const Articlehome = () => {
+  const [articleList, setArticleList] = useState([]);
+  const [input, setInput ] = useState("")
 
-    const [articleList, setArticleList] = useState([])
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const articles = await getArticles();
+      setArticleList(articles);
+    };
+    fetchArticles();
+  }, []);
 
-    useEffect(() => {
-        const fetchArticles = async () => {
-            const articles = await getArticles()
-            setArticleList(articles)
-        };
-        fetchArticles();
+  const handleSumbit = (e) => {
+    const fetchSearchedArtciles = async () => {
+      const searchedArticles = await getSearchedArticles(input);
+      setArticleList(searchedArticles);
     }
-    , []);
+    fetchSearchedArtciles();
+    e.preventDefault();
+  }
 
-    console.log(articleList)
-
-    return (
-        <div>
-            <Articles articles={articleList}/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <form
+        onSubmit={handleSumbit}
+      >
+        <label>
+          <button type="submit">Search</button>
+          <input onChange={(e) => setInput(e.target.value)} />
+        </label>
+      </form>
+      <Articles articles={articleList} />
+    </div>
+  );
+};
 
 export default Articlehome;
