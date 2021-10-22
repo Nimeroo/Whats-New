d6e458f90f5c37fe1ee519d6e7c5e26c5afcddd1
Nimeroo <nimero@DESKTOP-7MKA58K.localdomain>
2021-10-22 18:22:44 -0400

    Article details page route added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Articlehome from './Screens/ArticleHome/ArticleHome';
+import ArticleDetails from './Screens/ArticleDetails/ArticleDetails';
 
 function App() {
 
   return (
     <div className="App">
-      <Articlehome />
+      <Switch>
+        <Route exact path="/">
+          <Articlehome />
+        </Route>
+        <Route path="/:id">
+          <ArticleDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
