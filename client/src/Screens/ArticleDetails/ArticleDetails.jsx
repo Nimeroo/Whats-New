8dcee051959f811af39fd95a07fa3fca97da4b7e
Nimeroo <girou.joe@gmail.com>
2021-11-09 18:29:35 -0500

    Article details styling completed

--- client/src/Screens/ArticleDetails/ArticleDetails.jsx
+++ client/src/Screens/ArticleDetails/ArticleDetails.jsx
@@ -1,8 +1,10 @@
 import "./ArticleDetails.css";
 import thumbnailConverter from "../../Util/thumbnailConverter";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Container } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { Box } from "@mui/system";
 
 const ArticleDetails = ({ articles }) => {
   const [article, setArticle] = useState(null);
@@ -21,15 +23,22 @@ const ArticleDetails = ({ articles }) => {
   }
 
   return (
-    <div>
-      <Button onClick={() => history.goBack()}>Go Back</Button>
-      <Typography>{article.title}</Typography>
-      <Typography>{article.author}</Typography>
-      <img src={thumbnailConverter(article.category, article.image)}></img>
-      <Typography>{article.description}</Typography>
-      <a id="article-url" href={article.url}>
-        Article Page Here
-      </a>
+    <div id="article-details-container">
+      <Box id="article-details">
+        <ArrowBackIcon id="back-arrow" onClick={() => history.goBack()}>
+          Go Back
+        </ArrowBackIcon>
+        <Typography>{article.title}</Typography>
+        <Typography>{article.author}</Typography>
+        <img
+          id="article-image"
+          src={thumbnailConverter(article.category, article.image)}
+        ></img>
+        <Typography>{article.description}</Typography>
+        <a id="article-url" href={article.url}>
+          Full Article Page Here
+        </a>
+      </Box>
     </div>
   );
 };
