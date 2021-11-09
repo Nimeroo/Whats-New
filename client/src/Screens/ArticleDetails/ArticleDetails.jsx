a86d5d925983c808b2983c2a3eba76cd2393b1f1
Nimeroo <girou.joe@gmail.com>
2021-11-09 17:13:23 -0500

    article details page layout completed

--- client/src/Screens/ArticleDetails/ArticleDetails.jsx
+++ client/src/Screens/ArticleDetails/ArticleDetails.jsx
@@ -1,28 +1,35 @@
-import "./ArticleDetails.css"
-import thumbnailConverter from "../../Util/thumbnailConverter"
-import { useState, useEffect } from "react"
-import {useParams} from "react-router-dom"
+import "./ArticleDetails.css";
+import thumbnailConverter from "../../Util/thumbnailConverter";
+import { Typography, Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
 const ArticleDetails = ({ articles }) => {
   const [article, setArticle] = useState(null);
+  let history = useHistory();
   const { id } = useParams();
-  
+
   useEffect(() => {
     const articleInfo = articles.find((article) => article.id === id);
-    if(articleInfo){
+    if (articleInfo) {
       setArticle(articleInfo);
     }
-}, [id, articles]);
+  }, [id, articles]);
 
-if (!article) {
-    return <h1>Loading...</h1>
-}
+  if (!article) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <div>
-      <h2>{article.title}</h2>
-      <h5>{article.author}</h5>
+      <Button onClick={() => history.goBack()}>Go Back</Button>
+      <Typography>{article.title}</Typography>
+      <Typography>{article.author}</Typography>
       <img src={thumbnailConverter(article.category, article.image)}></img>
+      <Typography>{article.description}</Typography>
+      <a id="article-url" href={article.url}>
+        Article Page Here
+      </a>
     </div>
   );
 };
