ed1b95bf8297f8a45b0d4fea3a386ed63d7648fb
Nimeroo <girou.joe@gmail.com>
2021-11-11 05:16:08 -0500

    margin added to title

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div id="header">
-            <h2>Whats News</h2>
-        </div>
-    );
-}
+  return (
+    <div id="header">
+      <h2 id="title">Whats News</h2>
+    </div>
+  );
+};
 
 export default Header;
