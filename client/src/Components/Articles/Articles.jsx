47c7f300366b32d663d95fe9abfcc09cb0ca36a3
Nimeroo <girou.joe@gmail.com>
2022-05-05 17:38:48 -0400

    css for article list updated

--- client/src/Components/Articles/Articles.jsx
+++ client/src/Components/Articles/Articles.jsx
@@ -9,24 +9,31 @@ const Articles = ({ articles }) => {
       {articles.map((article) => {
         return (
           <Grid item xs={12} sm={6} md={4} key={article.id}>
-            <Link id="article-link" to={`/article/${article.id}`}>
-              <Paper elevation={1} id="article">
-                <Typography
-                  margin={2}
-                  maxHeight={50}
-                  whiteSpace="nowrap"
-                  overflow="hidden"
-                  textOverflow="ellipsis"
-                  fontSize={15}
-                  id="article-title"
-                >
-                  {article.title}
-                </Typography>
+            <Link className="article__link" to={`/article/${article.id}`}>
+              <Paper height="fit-content" elevation={3} id="article">
                 <img
-                  id="article-img"
+                  className="article__image"
                   alt={article.category[0]}
                   src={thumbnailConverter(article.category, article.image)}
                 ></img>
+                <Typography
+                  marginLeft={"1em"}
+                  marginRight={"0.5em"}
+                  marginBottom={"0"}
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  fontSize={15}
+                  fontWeight="bold"
+                  className="article__title"
+                >
+                  {article.title}
+                </Typography>
+                <p className="article__author">
+                  Written by: <b>{article.author}</b>
+                </p>
+                <p className="article__time">
+                  Time Published: {article.published}
+                </p>
               </Paper>
             </Link>
           </Grid>
