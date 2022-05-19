aa0df06b3868a53cdda4bbaaebef2e7d7dc3c063
Nimeroo <girou.joe@gmail.com>
2022-05-19 11:46:34 -0400

    Header changed to MUI component and conditional added for header box shadow

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "./Header.css";
+import { SearchBar } from "../SearchBar/SearchBar.jsx";
+import Logo from "../../assets/Whats-news-logo.png";
 import { Link } from "react-router-dom";
+import { Toolbar } from "@mui/material";
 
-const Header = () => {
+const Header = ({ fetchSearchedArticles }) => {
+  const [navShadow, setNavShadow] = useState(0);
+
+  window.onscroll = () => {
+    if (window.pageYOffset > 20) {
+      setNavShadow(3);
+    } else {
+      setNavShadow(0);
+    }
+  };
+
   return (
-    <div id="header">
-      <Link to="/home">
-        <h2 id="title">Whats News</h2>
+    <Toolbar sx={{ boxShadow: navShadow }} id="header">
+      <Link to="/">
+        <img className="header__logo" src={Logo} alt="logo"></img>
+        <h2 className="header__title">Whats News</h2>
       </Link>
-    </div>
+      <SearchBar fetchSearchedArticles={fetchSearchedArticles} />
+    </Toolbar>
   );
 };
 
