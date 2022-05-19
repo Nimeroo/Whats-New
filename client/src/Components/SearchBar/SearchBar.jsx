88a077e8ceca63cebc287b7ba2d493c41d9415d4
Nimeroo <girou.joe@gmail.com>
2022-05-19 11:47:38 -0400

    article searchbar turned into seperate component

---
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import "./SearchBar.css";
+import { IconButton, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+export const SearchBar = ({ fetchSearchedArticles }) => {
+  const [input, setInput] = useState("");
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    fetchSearchedArticles(input);
+  };
+
+  return (
+    <div className="search-area">
+      <form onSubmit={handleSumbit}>
+        <label>
+          <IconButton type="submit">
+            <SearchIcon fontSize="small" />
+          </IconButton>
+          <TextField
+            size="small"
+            label="Search topics and headlines"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
