e2a1a10b82322e259bfe96b0d17c5827d7ca53f2
Nimeroo <girou.joe@gmail.com>
2022-06-05 19:02:45 -0400

    links to home screen before searching

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./SearchBar.css";
 import { IconButton, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 export const SearchBar = ({ fetchSearchedArticles }) => {
+  let history = useNavigate();
   const [input, setInput] = useState("");
 
   const handleSumbit = (e) => {
     e.preventDefault();
+    history("/", { replace: true })
     fetchSearchedArticles(input);
   };
 
   return (
     <div className="search-area">
       <form onSubmit={handleSumbit}>
-          <IconButton type="submit">
-            <SearchIcon fontSize="small" />
-          </IconButton>
-          <TextField
-            size="small"
-            label="Search topics and headlines"
-            onChange={(e) => setInput(e.target.value)}
-          />
+        <IconButton type="submit">
+          <SearchIcon fontSize="small" />
+        </IconButton>
+        <TextField
+          size="small"
+          label="Search topics and headlines"
+          onChange={(e) => setInput(e.target.value)}
+        />
       </form>
     </div>
   );
