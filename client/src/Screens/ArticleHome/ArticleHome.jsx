4a9f3a2d76d2c90b962a4bbae0a359c80108687e
Nimeroo <girou.joe@gmail.com>
2022-06-12 04:49:04 -0400

    conditional added for if there are no available articles to queried topic

--- client/src/Screens/ArticleHome/ArticleHome.jsx
+++ client/src/Screens/ArticleHome/ArticleHome.jsx
@@ -2,22 +2,43 @@ import "./ArticleHome.css";
 import { Container } from "@mui/material";
 import Articles from "../../Components/Articles/Articles";
 import { ArticlesSkeleton } from "../../Components/ArticlesSkeleton/ArticlesSkeleton";
+import { Sidebar } from "../../Components/Sidebar/Sidebar";
 
-const ArticleHome = ({ articles, headlines, input }) => {
+const ArticleHome = ({
+  articles,
+  articleStatus,
+  headlines,
+  input,
+  categories,
+  fetchSearchedArticlesByCategory,
+}) => {
   return (
-    <div>
+    <div className="home__block">
+      {categories && (
+        <Container id="sidebar__block" fixed>
+          <Sidebar
+            fetchSearchedArticlesByCategory={fetchSearchedArticlesByCategory}
+            categories={categories}
+          />
+        </Container>
+      )}
+
       {articles && (
-        <Container className="article__block" fixed>
-          {headlines && <div className="headline__block__label">Headlines</div>}
+        <Container id="article__block" fixed>
+          {headlines && <div className="article__block__label">Headlines</div>}
           {input && (
-            <div className="headline__block__label">{`Articles related to "${input}"`}</div>
+            <div className="article__block__label">{`Articles related to "${input}"`}</div>
           )}
-          <Articles articles={articles} />
+          {articles.length === 0 && articleStatus === "ok" ? (
+            <h1>No articles related to this topic</h1>
+          ) : (
+            <Articles articles={articles} articleStatus={articleStatus} />
+          )}
         </Container>
       )}
 
       {!articles && (
-        <Container fixed>
+        <Container id="article-skeleton__block" fixed>
           <ArticlesSkeleton />
         </Container>
       )}
