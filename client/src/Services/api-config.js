ed9913079868f96456f3980e73aa984e5a44149f
Nimeroo <girou.joe@gmail.com>
2022-06-12 04:53:06 -0400

    function with category parameter added to domain implemented

--- client/src/Services/api-config.js
+++ client/src/Services/api-config.js
@@ -4,9 +4,9 @@ const domain = "https://api.currentsapi.services/v1/";
 
 export const getArticles = async () => {
   let resp = await axios.get(
-    `${domain}latest-news?language=en&apiKey=${process.env.REACT_APP_API_KEY}`
+    `${domain}latest-news?language=en&type=3&page_size=15&apiKey=${process.env.REACT_APP_API_KEY}`
   );
-  let articles = resp.data.news;
+  let articles = resp.data;
   return articles;
 };
 
@@ -14,6 +14,20 @@ export const getSearchedArticles = async (inputSearch) => {
   let resp = await axios.get(
     `${domain}search?keywords=${inputSearch}&language=en&apiKey=${process.env.REACT_APP_API_KEY}`
   );
-  let articles = resp.data.news;
+  let articles = resp.data;
+  return articles;
+};
+
+export const getArticleCategories = async () => {
+  let resp = await axios.get(`${domain}available/categories`);
+  let categories = resp.data.categories;
+  return categories;
+};
+
+export const getSearchedArticlesByCategories = async (inputSearch, category) => {
+  let resp = await axios.get(
+    `${domain}search?keywords=${inputSearch}&category=${category}&language=en&apiKey=${process.env.REACT_APP_API_KEY}`
+  );
+  let articles = resp.data;
   return articles;
 };
