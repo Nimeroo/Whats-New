1f70b590f218c6600c9c409230bc3dedbf30677a
Nimeroo <girou.joe@gmail.com>
2022-06-12 04:51:10 -0400

    function added to grab articles sorted by category

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { getArticles, getSearchedArticles } from "./Services/api-config";
+import {
+  getArticleCategories,
+  getArticles,
+  getSearchedArticles,
+  getSearchedArticlesByCategories,
+} from "./Services/api-config";
 import ArticleHome from "./Screens/ArticleHome/ArticleHome";
 import ArticleDetails from "./Screens/ArticleDetails/ArticleDetails";
 import { useEffect, useState } from "react";
@@ -8,25 +13,46 @@ import Header from "./Components/Header/Header";
 
 function App() {
   const [articles, setArticles] = useState(null);
+  const [articleStatus, setArticleStatus] = useState(null);
+  const [categories, setCategories] = useState(null);
   const [headlines, setHeadlines] = useState(true);
-  const [searchedInput, setSearchedInput] = useState("")
+  const [searchedInput, setSearchedInput] = useState("");
 
   useEffect(() => {
+    const fetchCategories = async () => {
+      const categoryList = await getArticleCategories();
+      if (categoryList) {
+        setCategories(categoryList);
+      }
+    };
     const fetchArticles = async () => {
       const articleList = await getArticles();
       if (articleList) {
-        setArticles(articleList);
+        setArticles(articleList.news);
+        setArticleStatus(articleList.status);
       }
     };
+    fetchCategories();
     fetchArticles();
   }, []);
 
   const fetchSearchedArticles = async (input) => {
     setArticles(null);
-    setSearchedInput(input)
+    setSearchedInput(input);
     const searchedArticles = await getSearchedArticles(input);
     input ? setHeadlines(false) : setHeadlines(true);
-    setArticles(searchedArticles);
+    setArticles(searchedArticles.news);
+    setArticleStatus(searchedArticles.status);
+  };
+
+  const fetchSearchedArticlesByCategory = async (category) => {
+    setArticles(null);
+    const filteredArticles = await getSearchedArticlesByCategories(
+      searchedInput,
+      category
+    );
+    setArticles(filteredArticles.news);
+    setArticleStatus(filteredArticles.status);
   };
 
   return (
@@ -35,7 +61,16 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<ArticleHome headlines={headlines} input={searchedInput} articles={articles} />}
+          element={
+            <ArticleHome
+              fetchSearchedArticlesByCategory={fetchSearchedArticlesByCategory}
+              headlines={headlines}
+              input={searchedInput}
+              articles={articles}
+              articleStatus={articleStatus}
+              categories={categories}
+            />
+          }
         />
         <Route
           path="/article/:id"
